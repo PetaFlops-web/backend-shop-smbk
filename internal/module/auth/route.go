87e1706b4805f26be606c95d9902cbf9cd83f093
Artt Vincii <otong123321@gmail.com>
2026-07-12 12:07:57 +0700

    feat: implement auth module with dependency injection and registration routes

--- internal/module/auth/route.go
+++ internal/module/auth/route.go
@@ -1,1 +1,14 @@
 package auth
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func (m *Module) RegisterRoutes(router fiber.Router, authMiddleware fiber.Handler) {
+	authRouter := router.Group("/auth")
+	authRouter.Post("/register", m.Controller.Register)
+	authRouter.Post("/login", m.Controller.Login)
+
+	authRouter.Use(authMiddleware)
+	authRouter.Get("/current", m.Controller.Current)
+}
