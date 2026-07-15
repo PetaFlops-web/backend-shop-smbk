0ae382c494653318005af3775192ab54d70a830a
Artt Vincii <otong123321@gmail.com>
2026-07-15 23:30:58 +0700

    feat(product): wire product module to main.go
    - Add module.go to define Product module, new controller/usecase
    - Add route.go for registering /api/products endpoints with auth
    - Wire Product module in main.go modules registry
    - Execute AutoMigrate for products table on startup

--- internal/module/product/route.go
+++ internal/module/product/route.go
@@ -1,1 +1,16 @@
 package product
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func (m *Module) RegisterRoutes(router fiber.Router, authMiddleware fiber.Handler) {
+	productRouter := router.Group("/products")
+	productRouter.Use(authMiddleware)
+
+	productRouter.Post("/", m.Controller.Create)
+	productRouter.Get("/", m.Controller.List)
+	productRouter.Get("/:id", m.Controller.Get)
+	productRouter.Put("/:id", m.Controller.Update)
+	productRouter.Delete("/:id", m.Controller.Delete)
+}
