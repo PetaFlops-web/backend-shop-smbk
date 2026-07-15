989942f7abec81eed8411994dba24ce9d8cbc043
Artt Vincii <otong123321@gmail.com>
2026-07-15 23:17:17 +0700

    feat(product): define SearchProductRequest model for product search functionality

--- internal/module/product/src/model/product_request.go
+++ internal/module/product/src/model/product_request.go
@@ -1,8 +1,1 @@
 package model
-
-type SearchProductRequest struct {
-	StoreId string `json:"store_id"`
-	Name    string `json:"name"`
-	Page    int    `json:"page"`
-	Size    int    `json:"size"`
-}
